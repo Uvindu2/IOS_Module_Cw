a5bd4befbcb2156ea2cb087ac46cab69320853d6
Uvindu Perera <uvinduperera97@gmail.com>
2024-03-16 17:15:49 +0530

    create project

---
+++ Clothing_App/Clothing_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clothing_App
+//
+//  Created by NIBM-LAB04-PC05 on 2024-03-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
