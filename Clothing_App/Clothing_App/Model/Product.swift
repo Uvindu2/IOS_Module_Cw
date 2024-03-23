b18d09657cf57e2bd8f8d3def813ae6a460d4338
Uvindu Perera <uvinduperera97@gmail.com>
2024-03-24 00:43:55 +0530

    fix ui for login registration and home screen filters

--- Clothing_App/Clothing_App/Model/Product.swift
+++ Clothing_App/Clothing_App/Model/Product.swift
@@ -14,11 +14,11 @@ struct Product: Identifiable {
     var price: Int
 }
 
-var productList = [Product(name: "Orange sweater", image: "sweater1", price: 54),
-                   Product(name: "Red wine sweater", image: "sweater2", price: 89),
-                   Product(name: "Sand sweater", image: "sweater3", price: 79),
-                   Product(name: "Sea sweater", image: "sweater4", price: 94),
-                   Product(name: "Cream sweater", image: "sweater5", price: 99),
-                   Product(name: "Beige sweater", image: "sweater6", price: 65),
-                   Product(name: "Grey sweater", image: "sweater7", price: 54),
-                   Product(name: "Mink sweater", image: "sweater8", price: 83)]
+var productList = [Product(name: "Orange sweater", image: "dress1", price: 54),
+                   Product(name: "Red wine sweater", image: "dress2", price: 89),
+                   Product(name: "Sand sweater", image: "dress3", price: 79),
+                   Product(name: "Sea sweater", image: "dress4", price: 94),
+                   Product(name: "Cream sweater", image: "dress5", price: 99),
+                   Product(name: "Beige sweater", image: "dress6", price: 65),
+                   Product(name: "Grey sweater", image: "dress7", price: 54),
+                   Product(name: "Mink sweater", image: "dress1", price: 83)]
--- Clothing_App/Clothing_App/ViewModels/LoginViewModel.swift
+++ Clothing_App/Clothing_App/ViewModels/LoginViewModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class LoginViewModel: ObservableObject {
+    @Published var username: String = ""
+    @Published var password: String = ""
+    
+    func login() -> Bool {
+        // Implement your login logic here
+        // For demonstration purposes, let's just check if username and password are not empty
+        return !username.isEmpty && !password.isEmpty
+    }
+}
+
